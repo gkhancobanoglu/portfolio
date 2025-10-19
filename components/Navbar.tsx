584661d5b871cc91e93bedfbc8a4ee41be72422e
Gökhan Çobanoğlu <gokhan@larsca.com>
2025-10-19 15:02:09 +0300

    feat: update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -17,7 +17,7 @@ export default function Navbar() {
   ];
 
   return (
-    <nav className="w-full bg-[#0B0F19] fixed top-0 left-0 z-50 border-b border-white/10">
+    <nav className="w-full bg-[#0B0F19] border-b border-white/10">
       <div className="max-w-6xl mx-auto px-6 py-4 flex items-center justify-between">
         {/* Sol: İmza */}
         <Link
